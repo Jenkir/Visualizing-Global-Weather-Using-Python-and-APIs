84327b109ea98055caee5cdb18e4920b1684983a
Jennifer Kirschnick Duffy <jenkirschnick@gmail.com>
2021-03-02 10:38:15 -0500

    Testing that api keys work

--- api_keys.py
+++ api_keys.py
@@ -1,38 +1,5 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# OpenWeatherMap API Key\n",
-    "weather_api_key = \"d6a77602667c0ccce427bc45026bcd82\"\n",
-    "\n",
-    "# Google API Key\n",
-    "g_key = \"AIzaSyA9iUMogMOnYiE8u3ZjmgvRk2M8xDRR5Z8\"\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
